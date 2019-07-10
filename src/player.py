f97cff006032ba8c86c336221bbf9a674845e210
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-07-10 17:28:58 -0600

    finished REPL loop.

--- src/player.py
+++ src/player.py
@@ -1,8 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-from room import Room
 
 
-class Player():
+class Player:
     def __init__(self, currentroom):
         self.currentroom = currentroom
